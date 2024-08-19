56566aba14552a7995640d94201c072d6f94d896
shogun187 <53176307+shogun187@users.noreply.github.com>
2024-08-20 00:24:39 +0800

    Add new projects

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div id="home-section">
     <particles />
-    <div style="position: relative">
+    <div style="position: relative;">
 
       <Menubar :model="items" style="font-weight: 700; font-size: 20px;"></Menubar>
       <br>
 
-      <div style="text-align: center">
+      <div style="text-align: center;">
         <Avatar image="./shaugn.jpg" shape="circle" style="height: 150px; width: 150px"></Avatar>
         <br>
         <span style="font-size: 40px">👋</span>
@@ -18,10 +18,10 @@
         <br><br>
         <Divider layout="vertical" style="height: 100px;" />
 
-        <div class="projects" style="margin: 0 auto; width: 50%">
+        <div class="projects" id="projects-section" style="margin: 0 auto; width: 50%">
           <h2>My Projects</h2>
 
-          <Card style="overflow: hidden">
+          <Card class="card">
             <template #header>
               <div style="margin-top: 10px">
                 <Image src="./linerlytica-table.png" width="800px"></Image>
@@ -35,7 +35,7 @@
                 Made With: &nbsp;
                 <Icon style="vertical-align: middle;" icon="logos:python" width="2em" height="2em" /> &nbsp;
                 <Icon style="vertical-align: middle;" icon="logos:selenium" width="2em" height="2em" /> &nbsp;
-                <Image style="vertical-align: middle" src="src/assets/beautiful-soup.svg" height="48" />
+                <Image style="vertical-align: middle" src="./beautiful-soup.svg" height="48" />
               </div>
             </template>
             <template #content>
@@ -48,7 +48,7 @@
 
           <br>
 
-          <Card style="overflow: hidden">
+          <Card class="card">
             <template #header>
               <div style="margin-top: 10px">
                 <Image src="./telegram-bot2.png"></Image>
@@ -66,7 +66,7 @@
               <Icon style="vertical-align: middle;" icon="logos:aws" width="2em" height="2em" /> &nbsp;
             </template>
             <template #content>
-              <div style="margin-top: 10px">
+              <div style="margin-top: 10px;">
                 Telegram chatbot to monitor HDB flat uptake for Bedok South Blossoms BTO launched in May 2023.
                 <br>
                 Allows users to keep a watchlist of flats and notify users whenever a HDB flat is taken.
@@ -78,31 +78,96 @@
               <Button style="margin-top: 10px" severity="contrast" rounded size="large" @click="redirectToLink('https://github.com/shogun187/BSB-BTO-Tracker')"><i class="pi pi-github"></i><strong>Project</strong></Button>
             </template>
           </Card>
+
+          <br>
+
+          <Card class="card">
+            <template #header>
+              <div style="margin-top: 10px">
+                <Image src="./chat-app.png" width="650" ></Image>
+              </div>
+            </template>
+            <template #title>
+              <h2>Chat App</h2>
+            </template>
+            <template #subtitle>
+              Made With:
+                <Icon style="vertical-align: middle; margin-right: 8px" icon="logos:nodejs-icon" width="2em" height="2em" />
+                <Icon style="vertical-align: middle; margin-right: 8px" icon="skill-icons:html" width="2em" height="2em" />
+                <Icon style="vertical-align: middle;" icon="skill-icons:css" width="2em" height="2em" />
+            </template>
+            <template #content>
+              <div style="margin-top: 10px;">
+                Designed and developed an online real-time chat room application
+                <br>
+                Allows users to create their own chat rooms and communicate using the Socket.io library’s room functionality
+                <br>
+                Built using Node.js, HTML and CSS
+              </div>
+            </template>
+            <template #footer>
+              <Button style="margin-top: 10px" severity="contrast" rounded size="large" @click="redirectToLink('https://github.com/shogun187/chat-app/tree/main')"><i class="pi pi-github"></i><strong>Project</strong></Button>
+            </template>
+          </Card>
+
+          <br>
+
+          <Card class="card">
+            <template #header>
+              <div style="margin-top: 10px">
+                <Image src="./uninurse.png" width="700" height="500"></Image>
+              </div>
+            </template>
+            <template #title>
+              <h2>UniNurse</h2>
+            </template>
+            <template #subtitle>
+              Made With:
+              <Icon style="vertical-align: middle; margin-left:2px" icon="devicon:java" width="2em" height="2em" />
+            </template>
+            <template #content>
+              <div style="margin-top: 10px;">
+                Desktop app designed for private nurses to manage their patient contacts
+                <br>
+                Allows tracking of patients' contact details, appointments, medications and medical conditions
+                <br>
+                Built using Java
+              </div>
+            </template>
+            <template #footer>
+              <Button style="margin-top: 10px" severity="contrast" rounded size="large" @click="redirectToLink('https://github.com/shogun187/tp')"><i class="pi pi-github"></i><strong>Project</strong></Button>
+            </template>
+          </Card>
+
+
+
+
         </div>
         <br>
 
         <Divider layout="vertical" style="height: 100px;" />
 
-        <div class="skills">
+        <div class="skills" id="skills-section">
           <h2>My Skills</h2>
 
           <div style="display: flex; justify-content: center; gap: 2em;">
-            <div style="display: flex; flex-direction: column; align-items: center;">
+
+            <div class="skill">
               <Icon icon="logos:javascript" width="3em" height="3em" />
               <h4>Javascript</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:typescript-icon" width="3em" height="3em" />
               <h4>TypeScript</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:vue" width="3em" height="3em" />
               <h4>Vue.js</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:nodejs-icon" width="3em" height="3em" />
               <h4>Node.js</h4>
             </div>
@@ -111,27 +176,27 @@
 
           <div style="display: flex; justify-content: center; gap: 2em;">
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:python" width="3em" height="3em" />
               <h4>Python</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:java" width="3em" height="3em" />
               <h4>Java</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:spring-icon" width="3em" height="3em" />
               <h4>Spring Boot</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="skill-icons:html" width="3em" height="3em" />
               <h4>HTML</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="skill-icons:css" width="3em" height="3em" />
               <h4>CSS</h4>
             </div>
@@ -140,27 +205,27 @@
 
           <div style="display: flex; justify-content: center; gap: 2em;">
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:postgresql" width="3em" height="3em" />
               <h4>PostgreSQL</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:mysql" width="3em" height="3em" />
               <h4>MySQL</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="devicon:mongodb" width="3em" height="3em" />
               <h4>MongoDB</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="skill-icons:docker" width="3em" height="3em" />
               <h4>Docker</h4>
             </div>
 
-            <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="skill">
               <Icon icon="logos:aws" width="3em" height="3em" />
               <h4>AWS</h4>
             </div>
@@ -172,12 +237,12 @@
 
         <Divider layout="vertical" style="height: 100px;" />
 
-        <div class="experience" style="width: 50%; margin: 0 auto">
+        <div class="experience" id="experience-section" style="width: 50%; margin: 0 auto">
           <h2>My Experience</h2>
           <Timeline :value="timeline" align="alternate">
 
             <template #marker>
-              <Avatar icon="pi pi-briefcase" shape="circle" />
+              <Avatar icon="pi pi-briefcase" shape="circle" style="background-color: #d9d9d9 !important" />
             </template>
 
             <template #opposite="slotProps">
@@ -189,7 +254,7 @@
 
             <template #content="slotProps">
               <div style="margin-bottom: 20px">
-                <Card style="border-radius: 3px; border: 2px black solid;">
+                <Card class="card" style="border-radius: 3px; border: 2px black solid;">
                   <template #title>
                     {{ slotProps.item.title }}
                   </template>
@@ -221,9 +286,9 @@
 
         <Divider layout="vertical" style="height: 100px;" />
 
-        <div class="education">
+        <div class="education" id="education-section">
           <h2>My Education</h2>
-          <Card style="width: 20%; margin: 0 auto;">
+          <Card class="card" style="width: 20%; margin: 0 auto;">
             <template #header>
               <div style="margin-top: 10px">
                 <Image src="./nus.png" height="100" />
@@ -253,6 +318,7 @@
 
       </div>
     </div>
+    <ScrollTop />
   </div>
 </template>
 
@@ -270,10 +336,10 @@ export default {
   data() {
     return {
       items: [
-        { label: 'Home' },
-        { label: 'Experience' },
-        { label: 'Projects' },
-        { label: 'Education' },
+        { label: 'Home', command: () => document.getElementById('home-section').scrollIntoView({ behavior: 'smooth' }) },
+        { label: 'Projects', command: () => document.getElementById('projects-section').scrollIntoView({ behavior: 'smooth' }) },
+        { label: 'Experience', command: () => document.getElementById('experience-section').scrollIntoView({ behavior: 'smooth' }) },
+        { label: 'Education', command: () => document.getElementById('education-section').scrollIntoView({ behavior: 'smooth' }) },
       ],
       timeline: [
         { title: 'Back-end Developer Intern', duration: 'May 2023 - Aug 2023', company: 'MCC Singapore',
@@ -299,18 +365,34 @@ export default {
 }
 
 </script>
+
 <style scoped>
+
+.skill {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 .p-menubar {
   justify-content: center;
   border: none !important;
   background: transparent !important;
+  color: black !important;
 }
 
+.card {
+  //color: black !important;
+  overflow: hidden
+}
 
+.p-scrolltop {
+  background: #8c8c8c !important;
+  border: none;
+}
 
-
-
-
-
-
+.p-timeline-event-marker {
+  background-color: red !important;
+  color: pink !important;
+}
 </style>
